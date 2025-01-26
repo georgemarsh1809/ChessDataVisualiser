a1a980cbd496c7c10dd4aff4863aa08eeed40614
Henry Walker <henry@Henrys-MBP.lan>
2025-01-26 17:53:05 +0000

    backend review

--- src/backend/utils/createDbTables.js
+++ src/backend/utils/createDbTables.js
@@ -1,15 +1,15 @@
 import sql from "../postgres.js";
 
 async function createDbTables() {
-
-    await sql`CREATE TABLE IF NOT EXISTS people (
+  // Update this with the columns from dataset
+  await sql`CREATE TABLE IF NOT EXISTS people (
         id smallint,
         name varchar(50),
         age smallint,
         PRIMARY KEY (id)
     );`;
 
-    sql.end()
+  sql.end();
 }
 
-createDbTables()
+createDbTables();
