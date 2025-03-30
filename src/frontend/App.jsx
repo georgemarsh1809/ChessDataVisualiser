b0a0d6cafb66100d6e96dbe7c4fba10d349ee278
George Marsh <georgemarsh1809@gmail.com>
2025-03-30 15:47:53 +0530

    Put all API calls into the same useEffect

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Layout } from "./components/Layout";
 import { DashboardTab } from "./components/DashboardTab";
 import { BrowserRouter, Routes, Route } from "react-router";
@@ -6,6 +6,7 @@ import { DataTab } from "./components/DataTab";
 import { useStore } from "./stateManagement/store";
 
 function App() {
+  // Importing all necessary state and state setter functions
   const { playerProfile } = useStore();
   const setFirstMoveData = useStore((state) => state.setFirstMoveData);
   const setResultData = useStore((state) => state.setResultData);
@@ -14,7 +15,11 @@ function App() {
 
 
   useEffect(() => {
-    const getData = async () => {
+    // This use effect runs every time the playerProfile state changes (whenever a new profile is loaded from the side modal)
+    // Since all the graphs are dependent on this state change, all API calls can be declared inside the same useEffect
+
+    // API Call for Total Games data
+    const getTotalGamesData = async () => {
       const res = await fetch(
         "http://localhost:3000/total-games?" +
         new URLSearchParams({
@@ -31,14 +36,9 @@ function App() {
         Number(resData[0]["count"])
       );
     };
-    getData();
-  }, [playerProfile]);
 
-  //First move
-  useEffect(() => {
-    console.log(playerProfile)
-
-    const getData = async () => {
+    // API Call for First Move data
+    const getFirstMoveData = async () => {
       const res = await fetch(
         "http://localhost:3000/first-move?" +
         new URLSearchParams({
@@ -59,12 +59,9 @@ function App() {
         }))
       );
     };
-    getData();
-  }, [playerProfile]);
 
-  //Outcome
-  useEffect(() => {
-    const getData = async () => {
+    // API Call for Outcome data
+    const getOutcomeData = async () => {
       const res = await fetch(
         "http://localhost:3000/outcome?" +
         new URLSearchParams({
@@ -83,11 +80,11 @@ function App() {
         }))
       );
     };
-    getData();
-  }, [playerProfile]);
 
-
-
+    getOutcomeData();
+    getFirstMoveData();
+    getTotalGamesData();
+  }, [playerProfile]); // The dependency array is set to the playerProfile state
 
 
   return (
